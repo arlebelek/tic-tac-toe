c31d7822b93ad5411b09eb9923afd38b73fa7ccb
arlebelek <macgaveur@gmail.com>
2015-11-17 15:48:27 +0100

    step 4 and 5

--- lib/app.jsx
+++ lib/app.jsx
@@ -8,16 +8,42 @@ var boxStyle = {
   'width':'100px'
 };
 
+
+
 var Box = React.createClass({
+    //step 4
+    'getInitialState': function onGetInitialState(){
+        return {
+            'value':this.props.initialValue
+        };
+    },
+    
+    //step 5 
+    componentWillMount: function(){
+       var oldthis = this;
+       this.timer = setInterval(function(){
+       
+       var old = oldthis.state.value;
+       var newV = old === 'X' ? 'O': 'X';
+       oldthis.setState({
+       value: newV
+       });
+       }, 300);
+       },
+           
+       componentWillUnmount: function(){
+   clearInterval(this.timer);},
+    
   /**
    * Render a HTML button
    * @return {ReactElement}
    */
   'render': function onRender () {
     return (
-      <button style={boxStyle}>{this.props.value}</button>
+      <button style={boxStyle}>{this.state.value}</button>
     );
   }
 });
 
-React.render(<Box value='X'/>, document.body);
+React.render(<Box initialValue={'X'}/>, document.body);
+
